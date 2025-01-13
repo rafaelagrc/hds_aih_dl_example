bb63da44ca28a4ce58a731aebd298817641af321
Rafaela Carvalho <rafaela.carvalho@fraunhofer.pt>
2025-01-13 14:26:01 +0000

    fix: delete conversion from bgr to rgb

--- utils.py
+++ utils.py
@@ -366,7 +366,7 @@ def oversample_minority_class_in_training_data(train_data, images_path):
             
             # Load the image using OpenCV
             img = cv2.imread(os.path.join(images_path, img_name))  # Load the image
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB format
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB format
             
             # Define a list of independent augmentation techniques
             augmentations = [
